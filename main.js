12cadd9acac9bee7a4735f29a103ff385b4271fc
vishaldotkhanna <vishalkhanna995@gmail.com>
2016-08-10 13:59:28 +0530

    No need for JS

--- main.js
+++
@@ -1,6 +0,0 @@
-window.onload = function()	{
-	for(var i = 5;i <= 8;i++)	{
-		console.log(i)
-		document.getElementById(toString(i)).onmouseover = document.getElementById(toString(i - 4)).style['visibility'] = 'visible';
-	}
-}
